c09c4b7b09cc913c4408bec0d59bbcfed13ea862
samiedaviss <samie.daviss@gmail.com>
2025-11-30 16:34:16 +0530

    Update CowController.java

--- src/main/java/com/scalesec/vulnado/CowController.java
+++ src/main/java/com/scalesec/vulnado/CowController.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 @RestController
 @EnableAutoConfiguration
 public class CowController {
-    @RequestMapping(value = "/cowsay", method = RequestMethod.GET)
+    @RequestMapping(value = "/cowsay")
     String cowsay(@RequestParam(defaultValue = "I love Linux!") String input) {
         return Cowsay.run(input);
     }
